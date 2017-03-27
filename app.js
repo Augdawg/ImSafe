fe5ab5109a24604564dc04c027813464ac6f5269
Kirin Bettadapur <kirinbetta97@gmail.com>
2017-03-27 15:26:51 -0400

    Env vars

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express')
 var app = express()
 var path = require("path");
 var request = require("request");
+const aws = require('aws-sdk');
 
 app.use('/style', express.static("assets/css/"))
 app.use('/fonts', express.static("assets/fonts/"))
@@ -15,8 +16,25 @@ app.get('/news', function(req, res) {
 	res.sendFile(path.join(__dirname + "/news.html"));
 })
 
+app.get('/rss', function(req, res) {
+	request({
+		method: 'GET',
+		url: 'https://news.google.com/?output=rss',
+	}, function(error, response, body) {
+		if (error) {
+			console.log("rss error");
+		} else {
+			res.send(body);
+		}
+	})
+})
+
 app.post('/', function(req, res) {
-	res.send(JSON.stringify(req.query.email));
+	let s3 = new aws.S3({
+		api_key: process.env.api_key
+	});
+	res.send(s3.api_key);
+	/*res.send(JSON.stringify(req.query.email));
 	var subscriber = JSON.stringify({
 		"email_address":req.query.email,
 		"status":"subscribed"
@@ -39,7 +57,7 @@ app.post('/', function(req, res) {
 			console.log(bodyObj.status);
 			console.log(bodyObj.email_address);
 		}
-	})
+	})*/
 })
 
 app.listen(process.env.PORT || 3000, function() {
